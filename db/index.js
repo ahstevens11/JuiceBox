20206c2077e19942d32bdfaadaf34ff577fe90b9
ahstevens11 <austin.cards20@gmail.com>
2021-04-15 14:04:05 -0500

    part 1 and ish

---
+++ db/index.js
@@ -0,0 +1,61 @@
+const { Client } = require('pg');
+require('dotenv').config();
+
+// console.log(process.env.DATABASE_URL)
+const client = new Client(process.env.DATABASE_URL);
+
+
+async function createUser({ username, password, name, location }) {
+    try {
+      const { rows } = await client.query(`
+        INSERT INTO users(username, password, name, location) 
+        VALUES($1, $2, $3, $4) 
+        ON CONFLICT (username) DO NOTHING 
+        RETURNING *;
+      `, [username, password, name, location]);
+  
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+  async function getAllUsers() {
+    const { rows } = await client.query(`SELECT id, username, name, location, active FROM users;`);
+  
+    return rows;
+  }
+
+  async function updateUser(id, fields = {}) {
+    // build the set string
+    const setString = Object.keys(fields).map(
+      (key, index) => `"${ key }"=$${ index + 1 }`
+    ).join(', ');
+  
+    // return early if this is called without fields
+    if (setString.length === 0) {
+      return;
+    }
+  
+    try {
+      const result = await client.query(`
+        UPDATE users
+        SET ${ setString }
+        WHERE id=${ id }
+        RETURNING *;
+      `, Object.values(fields));
+  
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+  module.exports = {  
+    client,
+    createUser,
+    getAllUsers,
+    updateUser,
+  }
+  
+ 
