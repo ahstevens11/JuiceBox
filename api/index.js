1759e515adc6500e6e5873cf73874730c0e5dc98
ahstevens11 <austin.cards20@gmail.com>
2021-04-22 17:57:10 -0500

    Finished part 2

--- api/index.js
+++ api/index.js
@@ -37,19 +37,18 @@ apiRouter.use(async (req, res, next) => {
   }
 });
 
+apiRouter.use((req, res, next) => {
+  if (req.user) {
+    console.log("User is set:", req.user);
+  }
+
+  next();
+});
 
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/posts', postsRouter)
 apiRouter.use('/tags', tagsRouter)
 
-
-apiRouter.use((req, res, next) => {
-    if (req.user) {
-      console.log("User is set:", req.user);
-    }
-  
-    next();
-  });
 
 
 apiRouter.use((error, req, res, next) => {
